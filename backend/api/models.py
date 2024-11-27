57bd187585433b32b62fe163b57cb3e9eed48257
Facundo Rodriguez <fakundor05@gmail.com>
2024-11-27 20:57:21 -0300

    modificacion de modelo

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     cantidad_disponible = models.IntegerField()
-    objects = models.Manager()
+    codigo = models.CharField(max_length=100)
     categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE, related_name='productos', default=1)
 
     def __str__(self):
